4e9064c4d243f26cea87f4c0f93feb87e03a6864
Olisajioke <chijioke914@gmail.com>
2024-04-09 13:49:57 +0100

    tasks 0, 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,29 @@
+
+#!/usr/bin/env python3
+"""A simple Flask app with internationalization support"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+@babel.localeselector
+def get_locale():
+    """match the best language supported by the application"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    return render_template('3-index.html', title=_('home_title'), header=_('home_header'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
